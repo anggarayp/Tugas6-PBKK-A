bc86151a339a7a393e2ff0a47f4acdf7e2787e6d
Anggara Yuda Pratama <anggara.yuda04@gmail.com>
2021-06-04 22:26:31 +0700

    tugas 6 profile

---
+++ application/views/about.php
@@ -0,0 +1,63 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>About Anggara</title>
+
+	<style type="text/css">
+
+	::selection { background-color: #E13300; color: white; }
+	::-moz-selection { background-color: #E13300; color: white; }
+
+	body {
+		background-color: #fff;
+		margin: 40px;
+		font: 16px/23px normal Helvetica, Arial, sans-serif;
+		color: #4F5155;
+	}
+
+	a {
+		color: #003399;
+		background-color: transparent;
+		font-weight: normal;
+	}
+
+	h1 {
+		color: #444;
+		background-color: transparent;
+		border-bottom: 1px solid #D0D0D0;
+		font-size: 25px;
+		font-weight: normal;
+		margin: 0 0 18px 0;
+		padding: 14px 15px 10px 15px;
+	}
+
+	#body {
+		margin: 0 15px 0 15px;
+	}
+
+	#container {
+		margin: 10px;
+		border: 1px solid #D0D0D0;
+		box-shadow: 0 0 8px #D0D0D0;
+	}
+	</style>
+</head>
+<body>
+
+<div id="container">
+    <div id="body">
+        <h1>About Anggara</h1>
+        <p>Nama Lengkap : Anggara Yuda Pratama</p>
+        <p>Tempat dan Tanggal Lahir : Surabaya, 04-07-2000</p>
+        <p>Alamat : Jl. Manyar Tirto Asri</p>
+        <p>Agama : Islam</p>
+        <p>Pekerjaan : Mahasiswa</p>
+        <p>Hobi : Gaming, Sepakbola, Basket</p>
+	</div>
+</div>
+
+</body>
+</html>
---
+++ application/views/contact.php
@@ -0,0 +1,60 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Contact Anggara</title>
+
+	<style type="text/css">
+
+	::selection { background-color: #E13300; color: white; }
+	::-moz-selection { background-color: #E13300; color: white; }
+
+	body {
+		background-color: #fff;
+		margin: 40px;
+		font: 16px/23px normal Helvetica, Arial, sans-serif;
+		color: #4F5155;
+	}
+
+	a {
+		color: #003399;
+		background-color: transparent;
+		font-weight: normal;
+	}
+
+	h1 {
+		color: #444;
+		background-color: transparent;
+		border-bottom: 1px solid #D0D0D0;
+		font-size: 25px;
+		font-weight: normal;
+		margin: 0 0 18px 0;
+		padding: 14px 15px 10px 15px;
+	}
+
+	#body {
+		margin: 0 15px 0 15px;
+	}
+
+	#container {
+		margin: 10px;
+		border: 1px solid #D0D0D0;
+		box-shadow: 0 0 8px #D0D0D0;
+	}
+	</style>
+</head>
+<body>
+
+<div id="container">
+    <div id="body">
+        <h1>Contact Me</h1>
+        <p>Instagram : anggarayp</p>
+        <p>Nomor HP : 081123456789</p>
+        <p>Email : anggara.yuda04@gmail.com</p>
+	</div>
+</div>
+
+</body>
+</html>
